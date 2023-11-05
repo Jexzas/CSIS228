341678f3f0fb72dffcd7431a272837c6f791dd5f
Jexzas <jdrmail2007@yahoo.com>
2023-11-05 13:33:24 -0600

    annotate everything and make sure the logic is sound

--- finalProject/script.js
+++ finalProject/script.js
@@ -1,3 +1,10 @@
+/**
+ * Author: Jesse Roberts
+ * Date completed: 11/5/23
+ */
+
+// I don't want this to live on the page until it's needed
+// Add recipes form
 let form =  `
     <form onsubmit="handleForm()">
         <div class="text-center"><h2>Add a new recipe</h2></div>
@@ -14,6 +21,10 @@ let form =  `
             <input type="textarea" class="form-control" id="post-ingredients">
         </div>
         <div class="form-group">
+            <label for="post-steps">List the Steps (separated by comma)</label>
+            <input type="textarea" class="form-control" id="post-steps">
+        </div>
+        <div class="form-group">
             <label for="post-rank">Difficulty Rating</label>
             <select class="form-control" id="post-rank">
                 <option value="1">1</option>
@@ -27,37 +38,57 @@ let form =  `
     </form>
 `
 
+// Establish a class for quickly building new recipes
+
 class Recipe {
     title;
     content;
     rank;
     ingredients = [];
+    steps = [];
     type = "blog";
 };
 
+// Resets the page without actually refreshing so that everything can exist on one page
+
 const reload = () => {
-    let home = pages.findIndex((x) => x = "Home")
+    // establish my two static pages as first two in pages array
+    let home = 0
+    let inputPage = 1;
+
+    // Empty the homepage if there's something in that content block
     if (pages[home].content) {
         pages[home].content = ""
     }
+
+    // Functions to build the static pages when clicked
     pages[home].content = createArchive();
-    pages[1].content = createForm();
+    pages[inputPage].content = createForm();
+
+    // Create the main content div
     let currentContent = document.createElement("div");
     if (activePage == 0 || activePage == 1) {
         currentContent.innerHTML = pages[activePage].content.outerHTML;
     } else if (pages[activePage].type == "blog") {
+        // Build recipe pages dynamically, so they don't exist in the DOM until they're needed
         let title = document.createElement("h1");
         title.innerHTML = pages[activePage].title;
         currentContent.appendChild(title);
         let rank = document.createElement("span");
         rank.id = "stars";
         rank.innerHTML = "<h3>Difficulty: </h3>";
+        // Use the rating to add some fontawesome icons
         for (let i = 0; i < pages[activePage].rank; i++) {
             let star = document.createElement("i");
             star.classList = "fa-solid fa-star";
             rank.appendChild(star);
         }
         currentContent.appendChild(rank);
+        // The blog section
+        let description = document.createElement("article");
+        description.innerHTML += pages[activePage].content;
+        currentContent.appendChild(description);
+        // self explanatory building of the ingredients section
         let ingredientsList = document.createElement("ul");
         ingredientsList.classList = "list-unstyled";
         ingredientsList.innerHTML += "<h3><strong>Ingredients:</strong></h3>";
@@ -67,13 +98,19 @@ const reload = () => {
             ingredientsList.appendChild(currentIng);
         }
         currentContent.appendChild(ingredientsList);
-        let description = document.createElement("article");
-        let descHeading = document.createElement("h3");
-        descHeading.textContent = "Directions:";
-        description.appendChild(descHeading);
-        description.innerHTML += pages[activePage].content;
-        currentContent.appendChild(description);
+        // building the steps section
+        let stepsHeading = document.createElement("h4");
+        stepsHeading.textContent = "Directions:";
+        let stepsList = document.createElement("ol");
+        for (step of pages[activePage].steps) {
+            let currentStep = document.createElement("li");
+            currentStep.innerHTML = step;
+            stepsList.appendChild(currentStep);
+        }
+        currentContent.appendChild(stepsList);
+
     }
+    // empty the display before adding current display
     if (content.hasChildNodes()) {
         content.removeChild(content.firstChild);
     }
@@ -86,16 +123,6 @@ const recipesLink = document.getElementById("recipesLink");
 const addRecipeLink = document.getElementById("addRecipeLink");
 let pages = [];
 const content = document.getElementById("display");
-
-// Add event listeners for the menu items
-recipesLink.addEventListener("click", () => {
-    activePage = 0;
-    reload();
-})
-addRecipeLink.addEventListener("click", () => {
-    activePage = 1;
-    reload();
-})
 
 // Create the static pages
 const createArchive = () => {
@@ -110,6 +137,8 @@ const createArchive = () => {
     archive.appendChild(subheading);
     let listArch = document.createElement("ul");
     listArch.classList = "list-unstyled row g-0 col-12";
+
+    // List all recipes on the homepage
     for (let page of pages) {
         if (page.type == "blog") {
             let post = document.createElement("li");
@@ -134,12 +163,25 @@ const createForm = () => {
     return formPage;
 }
 
-// Build the website dynamically
+// Build the initial website dynamically
 const init = () => {
+    // establish my two static pages as first two in pages array
+    let home = 0
+    let inputPage = 1;
+    // Add event listeners for the menu items
+    recipesLink.addEventListener("click", () => {
+        activePage = 0;
+        reload();
+    })
+    addRecipeLink.addEventListener("click", () => {
+        activePage = 1;
+        reload();
+    })
     content.innerHTML = "";
     !localStorage.main ? localStorage.setItem("main", "") : "";
     let archive = createArchive();
     let formPage = createForm();
+    // Building the static pages as object literals because they need to not be of type "blog"
     let mainPage = {
         "title": "Home",
         "content": archive.innerHTML,
@@ -154,15 +196,10 @@ const init = () => {
         "ingredients": [],
         "type": "static"
     }
+    // Empty page array? Create new statics
     if (pages.length == 0) {
         pages.push(mainPage, addPage);
     }
-    recipesLink.addEventListener("click", () => {
-        activePage = 0;
-    })
-    addRecipeLink.addEventListener("click", () => {
-        activePage = 1;
-    })
     let currentContent = document.createElement("div");
     currentContent.innerHTML = pages[activePage].content;
     content.appendChild(currentContent);
@@ -178,18 +215,19 @@ window.addEventListener("load", init);
 
 // Add..post...
 
-const addPost = (title, content, rank, ingredients) => {
+const addPost = (title, content, rank, ingredients, steps) => {
     let thisPost = new Recipe;
     thisPost.title = title;
     thisPost.content = content;
     thisPost.rank = rank;
     thisPost.ingredients = ingredients;
+    thisPost.steps = steps;
     // Adds post to dynamic storage
     pages.push(thisPost);
     // Syncs local storage with dynamic
     localStorage.clear();
     localStorage.setItem("main", JSON.stringify(pages));
-    activePage = 0;
+    activePage = 1;
     reload();
 }
 
@@ -205,8 +243,13 @@ function handleForm() {
         arrayIngredients.forEach((el) => {
             el = el.trim();
         })
+        let unhandledSteps = document.getElementById("post-steps").value;
+        let arraySteps = unhandledSteps.split(",");
+        arraySteps.forEach((el) => {
+            el = el.trim;
+        })
     
-        addPost(title, content, rank, arrayIngredients);
+        addPost(title, content, rank, arrayIngredients, arraySteps);
     } catch (e) {
         console.log(e);
     }
